202ff3987399bf00d1123c012f4786c372a66ef3
noahspoling <noahspoling@gmail.com>
2025-01-12 20:06:17 +0000

    Deploying to gh-pages from @ noahspoling/platformer-game@11015e109bc2cbe5fd46927e5c60532be7fb7ea5 🚀

---
+++ lib/extern/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/work/platformer-game/platformer-game/lib/extern/raylib
+# Build directory: /home/runner/work/platformer-game/platformer-game/build_web/lib/extern/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
---
+++ lib/extern/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/work/platformer-game/platformer-game/lib/extern/raylib/src
+# Build directory: /home/runner/work/platformer-game/platformer-game/build_web/lib/extern/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
